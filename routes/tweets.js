915c6b0efb13bb5be022f826c4421cc992923dca
tkenned2020 <tkenned2020@outlook.com>
2021-05-26 16:34:43 -0400

    phase 3 in progress

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,9 +1,37 @@
 const express = require("express");
 
+const db = require("../db/models");
 const router = express.Router();
+const { Tweet } = db;
 
-router.get("/", (req, res) => {
-	res.json({ message: "test tweets index" });
-});
+const asyncHandler = (handler) => (req, res, next) =>
+  handler(req, res, next).catch(next);
 
+function tweetNotFoundError(id) {
+  const err = new Error(`tweet of ${id} could not be found.`);
+  err.status = 404;
+  err.title = "tweet not found.";
+  return err;
+}
+
+router.get(
+  "/tweets",
+  asyncHandler(async (req, res, next) => {
+    const tweets = await Tweet.findAll();
+    res.json({ tweets });
+  })
+);
+
+router.get(
+  "/tweets/:id(\\d+)",
+  asyncHandler(async (req, res, next) => {
+    const id = req.params.id;
+    const tweet = await Tweet.findByPk(id);
+    if (tweet) {
+      res.json(tweet);
+    } else {
+      next(tweetNotFoundError(id));
+    }
+  })
+);
 module.exports = router;
