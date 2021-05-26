3e250437bab330b90944e3a66222b704a7bc6f69
Eduardo <eduardosh1105@gmail.com>
2021-05-26 11:56:51 -0600

    Phase 1 done

--- db/models/tweet.js
+++ db/models/tweet.js
@@ -1,10 +1,17 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-    message: DataTypes.STRING
-  }, {});
-  Tweet.associate = function(models) {
-    // associations can be defined here
-  };
-  return Tweet;
+	const Tweet = sequelize.define(
+		"Tweet",
+		{
+			message: {
+				type: DataTypes.STRING(280),
+				allowNull: false,
+			},
+		},
+		{}
+	);
+	Tweet.associate = function (models) {
+		// associations can be defined here
+	};
+	return Tweet;
 };
