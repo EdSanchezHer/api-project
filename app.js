98801de15702452a65ca4a00f492a29f963ac3d3
tkenned2020 <tkenned2020@outlook.com>
2021-05-26 17:45:26 -0400

    phase 4 part one in progress

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 const { environment } = require("./config");
 const tweetsRouter = require("./routes/tweets");
 const indexRouter = require("./routes/index");
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:4000" }));
 app.use(indexRouter);
 app.use(tweetsRouter);
 
--- routes/tweets.js
+++ routes/tweets.js
@@ -34,4 +34,6 @@ router.get(
     }
   })
 );
+
+
 module.exports = router;
