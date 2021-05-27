b03ded23519f51f94c9645edae6f29829f00e3de
tkenned2020 <tkenned2020@outlook.com>
2021-05-27 15:48:12 -0400

    fixed errors for phase 1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
 const { environment } = require("./config");
 const tweetsRouter = require("./routes/tweets");
 const indexRouter = require("./routes/index");
@@ -11,28 +11,28 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:4000" }));
 app.use(indexRouter);
-app.use(tweetsRouter, "/tweets");
-app.use(usersRouter, "/user");
+app.use("/tweets", tweetsRouter);
+app.use("/users", usersRouter);
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
-	const err = new Error("The requested resource couldn't be found.");
-	err.status = 404;
-	next(err);
+  const err = new Error("The requested resource couldn't be found.");
+  err.status = 404;
+  next(err);
 });
 
 // Custom error handlers.
 
 // Generic error handler.
 app.use((err, req, res, next) => {
-	res.status(err.status || 500);
-	const isProduction = environment === "production";
-	res.json({
-		title: err.title || "Server Error",
-		message: err.message,
-		errors: err.errors,
-		stack: isProduction ? null : err.stack,
-	});
+  res.status(err.status || 500);
+  const isProduction = environment === "production";
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const { check } = require("express-validator");
 const { asyncHandler, handleValidationErrors } = require("../utils");
 const bcrypt = require("bcryptjs");
 const { User } = require("../db/models");
+const { getUserToken } = require("../auth.js");
 
 const router = express.Router();
 
@@ -28,7 +29,7 @@ router.post(
   asyncHandler(async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const users = await User.create({ username, email, hashedPassword });
+    const user = await User.create({ username, email, hashedPassword });
 
     const token = getUserToken(user);
     res.status(201).json({
