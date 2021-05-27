01e8fbec19769932bee990543c7502f3be8e3829
Eduardo <eduardosh1105@gmail.com>
2021-05-27 15:45:07 -0600

    Finished phase 4

--- routes/users.js
+++ routes/users.js
@@ -8,35 +8,61 @@ const { getUserToken } = require("../auth.js");
 const router = express.Router();
 
 const validateUsername = check("username")
-  .exists({ checkFalsy: true })
-  .withMessage("Please provide a username");
+	.exists({ checkFalsy: true })
+	.withMessage("Please provide a username");
 
 const validateEmailAndPassword = [
-  check("email")
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage("Please provide a valid email."),
-  check("password")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a password."),
+	check("email")
+		.exists({ checkFalsy: true })
+		.isEmail()
+		.withMessage("Please provide a valid email."),
+	check("password")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a password."),
 ];
 
 router.post(
-  "/",
-  validateUsername,
-  validateEmailAndPassword,
-  handleValidationErrors,
-  asyncHandler(async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ username, email, hashedPassword });
+	"/",
+	validateUsername,
+	validateEmailAndPassword,
+	handleValidationErrors,
+	asyncHandler(async (req, res, next) => {
+		console.log(req.body);
+		const { username, email, password } = req.body;
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const user = await User.create({ username, email, hashedPassword });
+		console.log(user);
 
-    const token = getUserToken(user);
-    res.status(201).json({
-      user: { id: user.id },
-      token,
-    });
-  })
+		const token = getUserToken(user);
+		res.status(201).json({
+			user: { id: user.id },
+			token,
+		});
+	})
+);
+
+router.post(
+	"/token",
+	validateEmailAndPassword,
+	asyncHandler(async (req, res, next) => {
+		const { email, password } = req.body;
+		const user = await User.findOne({
+			where: {
+				email,
+			},
+		});
+
+		if (!user || !user.validatePassword(password)) {
+			const err = new Error("Login failed");
+			err.status = 401;
+			err.title = "Login failed";
+			err.errors = ["The provided credentials were invalid."];
+			return next(err);
+		}
+
+		const token = getUserToken(user);
+		res.json({ token, user: { id: user.id } });
+	})
 );
 
 module.exports = router;
